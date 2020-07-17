e02136d7bcc413784fe52b31076b3acd6aabbece
Andre <andrey.strelkovv@gmail.com>
2020-07-17 16:34:43 +0100

    Menu class skelleton

--- Biom_Constructor/Biom_Constructor/Menu.cpp
+++ Biom_Constructor/Biom_Constructor/Menu.cpp
@@ -1,1 +1,33 @@
 #include "Menu.h"
+
+wxBEGIN_EVENT_TABLE(Menu, wxMDIParentFrame)
+EVT_MENU(10001, Menu::menuNew)
+EVT_MENU(10002, Menu::menuOpen)
+EVT_MENU(10003, Menu::menuSave)
+EVT_MENU(10004, Menu::menuExit)
+wxEND_EVENT_TABLE()
+
+
+Menu::Menu() : wxMDIParentFrame(nullptr, wxID_ANY, "ANDY", wxPoint(100, 100), wxSize(800, 600))
+{
+}
+
+Menu::~Menu()
+{
+}
+
+void Menu::menuNew(wxCommandEvent& evt)
+{
+}
+
+void Menu::menuOpen(wxCommandEvent& evt)
+{
+}
+
+void Menu::menuSave(wxCommandEvent& evt)
+{
+}
+
+void Menu::menuExit(wxCommandEvent& evt)
+{
+}
--- Biom_Constructor/Biom_Constructor/Menu.h
+++ Biom_Constructor/Biom_Constructor/Menu.h
@@ -1,5 +1,22 @@
 #pragma once
-class Menu
-{
-};
+#include "wx/wx.h"
 
+class Menu : public wxMDIParentFrame
+{
+public:
+	Menu();
+	~Menu();
+
+private:
+	wxToolBar* toolBar = nullptr;
+	wxMenuBar* menuBar = nullptr;
+
+	void menuNew(wxCommandEvent& evt);
+	void menuOpen(wxCommandEvent& evt);
+	void menuSave(wxCommandEvent& evt);
+	void menuExit(wxCommandEvent& evt);
+
+	void selectColour(wxCommandEvent& evt);
+
+	wxDECLARE_EVENT_TABLE();
+};
