299923998c4e3ca5a00c80a04cf043af8ec251f1
Andre <andrey.strelkovv@gmail.com>
2020-07-17 16:43:10 +0100

    app skelleton

--- Biom_Constructor/Biom_Constructor/App.cpp
+++ Biom_Constructor/Biom_Constructor/App.cpp
@@ -1,1 +1,18 @@
 #include "App.h"
+
+wxIMPLEMENT_APP(App);
+
+App::App()
+{
+}
+
+App::~App()
+{
+}
+
+bool App::OnInit()
+{
+	menu = new Menu();
+	menu->Show();
+	return true;
+}
--- Biom_Constructor/Biom_Constructor/App.h
+++ Biom_Constructor/Biom_Constructor/App.h
@@ -1,5 +1,15 @@
 #pragma once
-class App
-{
-};
 
+#include "wx/wx.h"
+#include "Menu.h"
+
+class App : public wxApp
+{
+public:
+	App();
+	~App();
+private:
+	Menu* menu = nullptr;
+public:
+	virtual bool OnInit();
+};
