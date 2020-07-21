bd0ecef5db813cb97607eb788fbfcd4e5ecd1dae
Andre <andrey.strelkovv@gmail.com>
2020-07-21 12:30:47 +0100

    menu bar

--- Biom_Constructor/Biom_Constructor/Menu.cpp
+++ Biom_Constructor/Biom_Constructor/Menu.cpp
@@ -1,15 +1,25 @@
 #include "Menu.h"
 
 wxBEGIN_EVENT_TABLE(Menu, wxMDIParentFrame)
-EVT_MENU(10001, Menu::menuNew)
-EVT_MENU(10002, Menu::menuOpen)
-EVT_MENU(10003, Menu::menuSave)
-EVT_MENU(10004, Menu::menuExit)
+	EVT_MENU(10001, Menu::menuNew)
+	EVT_MENU(10002, Menu::menuOpen)
+	EVT_MENU(10003, Menu::menuSave)
+	EVT_MENU(10004, Menu::menuExit)
 wxEND_EVENT_TABLE()
 
 
 Menu::Menu() : wxMDIParentFrame(nullptr, wxID_ANY, "ANDY", wxPoint(100, 100), wxSize(800, 600))
 {
+	menuBar = new wxMenuBar();
+	this->SetMenuBar(menuBar);
+
+	wxMenu* menuFile = new wxMenu();
+	menuFile->Append(10001, "New");
+	menuFile->Append(10002, "Open");
+	menuFile->Append(10003, "Save");
+	menuFile->Append(10004, "Exit");
+
+	menuBar->Append(menuFile, "File");
 }
 
 Menu::~Menu()
